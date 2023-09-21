de12a2ec4e61272f5190be7f8e7acc7c7a6150ae
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-09-21 03:20:19 +0300

    updated models/user file

--- models/user.py
+++ models/user.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-import os
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
+
     __tablename__ = 'users'
-    email = Column(
-            String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    password = Column(
-            String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    first_name = Column(
-            String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    last_name = Column(
-            String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-            'Place',
-            cascade="all, delete, delete-orphan",
-            backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
-    reviews = relationship(
-            'Review',
-            cascade="all, delete, delete-orphan",
-            backref='user'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
