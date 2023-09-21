7abb5cd8767fdfcbcbcc22fadf2fa063ff3b1e50
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-09-21 03:33:45 +0300

    🚑️🚑️ critical hotfix

--- models/user.py
+++ models/user.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
 
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+        places = relationship(
+            'Place',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+        )
+
+        reviews = relationship(
+            'Review',
+            cascade="all, delete, delete-orphan",
+            backref='user'
+        )
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
