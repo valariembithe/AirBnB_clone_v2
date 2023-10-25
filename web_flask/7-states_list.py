0199f0d00de1b58778b459eadc7507f80537d6ed
valariembithe <valmbithe691@gmail.com>
2023-10-25 04:44:24 -0700

    Tasks

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application"""
+from flask import Flask
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def close_db():
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+@app.route('/states_list')
+def states_list():
+    """ a states list """
+    states = Storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
